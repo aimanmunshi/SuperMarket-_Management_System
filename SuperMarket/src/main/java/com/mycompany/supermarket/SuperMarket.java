c7c76e4f51df48aa928a2883d62181e5d4060281
aimanmunshi <150712123+aimanmunshi@users.noreply.github.com>
2024-09-09 22:34:05 +0530

    Add files via upload

---
+++ SuperMarket/src/main/java/com/mycompany/supermarket/SuperMarket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermarket;
+
+/**
+ *
+ * @author yashr
+ */
+public class SuperMarket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
