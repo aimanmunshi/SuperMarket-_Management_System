741fd3e3d2aa8c7f9515ef3292a0a16becaa30b8
aimanmunshi <150712123+aimanmunshi@users.noreply.github.com>
2024-09-11 00:14:14 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/supermarket/SuperMarket.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermarket;
+
+import Home.LoginPage;
+
+/**
+ *
+ * @author yashr
+ */
+public class SuperMarket {
+
+    public static void main(String[] args) {
+         new LoginPage().setVisible(true);
+    }                
+    }
+
